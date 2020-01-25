fbdbbab0043fa33526bfe48867b610e1e8c199f0
Sergey Razdolyanskiy <grafcorp01@gmail.com>
2020-01-25 17:52:48 +0300

    Add connect DB Mongo

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+connectDB();
 app.get('/', (req, res) => res.send('API Work'));
 
 const PORT = process.env.PORT || 5000;
