8cf6badbf5d026e612f1fffea0e2a9ad5d82883f
Sergey Razdolyanskiy <grafcorp01@gmail.com>
2020-01-26 20:42:01 +0300

    Add validate user route

--- routers/api/users.js
+++ routers/api/users.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
 
-// @route  GET api/users
-// @desc   test route
+// @route  POST api/users
+// @desc   Register Users
 // @access Public
-router.get('/', (req, res) => res.send('Users rotute'));
+router.post(
+	'/',
+	[
+		check('name', 'Name is Reqired').notEmpty(),
+		check('email', 'Please enter valid email').isEmail(),
+		check('password', 'Min lenght 6 char').isLength({ min: 6 })
+	],
+	(req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		res.send('Users rotute');
+	}
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const connectDB = require('./config/db');
 const app = express();
 
 connectDB();
+app.use(express.json({ extended: false }));
 app.get('/', (req, res) => res.send('API Work'));
 
 app.use('/api/users/', require('./routers/api/users'));
