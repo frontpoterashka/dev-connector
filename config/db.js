4f761bafe689c5bc75f323f55968d4b83fea0930
Sergey Razdolyanskiy <grafcorp01@gmail.com>
2020-01-27 00:12:41 +0300

    Connect register user to Mongo

--- config/db.js
+++ config/db.js
@@ -4,17 +4,18 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(db, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true
-    });
-    console.log('DB Connected');
-  } catch (error) {
-    console.log(error.message);
+	try {
+		await mongoose.connect(db, {
+			useUnifiedTopology: true,
+			useNewUrlParser: true,
+			useCreateIndex: true
+		});
+		console.log('DB Connected');
+	} catch (error) {
+		console.log(error.message);
 
-    process.exit(1);
-  }
+		process.exit(1);
+	}
 };
 
 module.exports = connectDB;
--- routers/api/users.js
+++ routers/api/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+const User = require('../../models/User');
 
 const router = express.Router();
 
@@ -13,13 +17,45 @@ router.post(
 		check('email', 'Please enter valid email').isEmail(),
 		check('password', 'Min lenght 6 char').isLength({ min: 6 })
 	],
-	(req, res) => {
+	async (req, res) => {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
 		}
 
-		res.send('Users rotute');
+		try {
+			const { name, email, password } = req.body;
+
+			let user = await User.findOne({ email });
+			if (user) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: 'User already exist' }] });
+			}
+
+			const avatar = gravatar.url({
+				s: '200',
+				r: 'pg',
+				d: 'mm'
+			});
+
+			user = new User({
+				name,
+				email,
+				avatar,
+				password
+			});
+
+			const salt = await bcrypt.genSalt(10);
+
+			user.password = await bcrypt.hash(password, salt);
+			await user.save();
+
+			return res.send('Users rerister');
+		} catch (error) {
+			console.error(errors.message);
+			return res.status(500).send('Server error');
+		}
 	}
 );
 
