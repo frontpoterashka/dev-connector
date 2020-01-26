e3dcd7ddc9c49d9273b9d054015105f178545fc7
Sergey Razdolyanskiy <grafcorp01@gmail.com>
2020-01-26 19:34:22 +0300

    Add routes api

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 connectDB();
 app.get('/', (req, res) => res.send('API Work'));
 
+app.use('/api/users/', require('./routers/api/users'));
+app.use('/api/auth/', require('./routers/api/auth'));
+app.use('/api/profile/', require('./routers/api/profile'));
+app.use('/api/posts/', require('./routers/api/posts'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server start on ${PORT} - port`));
