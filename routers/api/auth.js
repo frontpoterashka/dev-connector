f7c00324f990e3766129aa4d08ccd30d377a7c6e
Sergey Razdolyanskiy <grafcorp01@gmail.com>
2020-02-03 16:34:11 +0300

    Create login route

--- routers/api/auth.js
+++ routers/api/auth.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
 const auth = require('../../middleware/auth');
@@ -17,5 +21,63 @@ router.get('/', auth, async (req, res) => {
 		res.status(500).json('Server error');
 	}
 });
+
+// @route  POST api/auth
+// @desc   Authenticated
+// @access Public
+router.post(
+	'/',
+	[
+		check('email', 'Please enter valid email').isEmail(),
+		check('password', 'Password is reqired').exists()
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		try {
+			const { email, password } = req.body;
+
+			const user = await User.findOne({ email });
+
+			if (!user) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: 'Invalid credential' }] });
+			}
+
+			bcrypt.compare(password, user.password, (err, hash) => {
+				if (!hash) {
+					return res
+						.status(400)
+						.json({ errors: [{ msg: 'Invalid credential' }] });
+				}
+
+				const payload = {
+					id: user.id
+				};
+
+				jwt.sign(
+					payload,
+					config.get('jwtSecert'),
+					{
+						expiresIn: 3600
+					},
+					(errToken, token) => {
+						if (errToken) {
+							throw errToken;
+						}
+						return res.json({ token });
+					}
+				);
+			});
+		} catch (error) {
+			console.error(errors.message);
+			return res.status(500).send('Server error');
+		}
+	}
+);
 
 module.exports = router;
