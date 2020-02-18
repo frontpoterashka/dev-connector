f637c1bfd7f4216a2bf579c812ca85b3e97cf2b5
Sergey Razdolyanskiy <grafcorp01@gmail.com>
2020-02-18 10:36:39 +0300

    Create get profile route

--- routers/api/profile.js
+++ routers/api/profile.js
@@ -1,10 +1,30 @@
 const express = require('express');
 
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route  GET api/profile
-// @desc   test route
-// @access Public
-router.get('/', (req, res) => res.send('Profile rotute'));
+const Profile = require('../../models/Profile');
+
+// @route  GET api/profile/me
+// @desc   Get current users profile
+// @access Private
+router.get('/me', auth, async (req, res) => {
+	try {
+		Profile.findOne({ user: req.user.id })
+			.populate('user', ['name', 'avatar'])
+			.then(profile => {
+				if (!profile) {
+					return res
+						.status(400)
+						.json({ msg: 'There is not profile for this user' });
+				}
+				return res.json(profile);
+			})
+			.catch(err => res.status(404).json(err));
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).send('Server error');
+	}
+});
 
 module.exports = router;
